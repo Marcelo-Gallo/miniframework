7bfb434c002aa480a6263f893d98d5188b708a68
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-09-24 19:18:53 -0300

    Miniframework - Framework de cunho pessoal que será desenvolvido no curso domineWeb 2.0

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
-    echo 'Está tudo funcionando';
+    echo 'Miniframework rodando na porta 8080';
+    echo '<br>';
+    echo '(php -S localhost:8080) -> comando que inicializa o servidor na porta 8080 da rede.'
 
 ?>
