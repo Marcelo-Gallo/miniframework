a1de4d9b4e824873654bc5b4901c712928d053e3
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-09-24 21:48:11 -0300

    miniframework - setando composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'meu-vendor/miniframework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7de2a317fc66ed05bd6656584850e835ca140bca',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'meu-vendor/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7de2a317fc66ed05bd6656584850e835ca140bca',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
