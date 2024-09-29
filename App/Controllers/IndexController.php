1947e20f45e6af9c226bca2418906a87fa461d71
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-09-28 22:37:57 -0300

    miniframework - Adicionando infos para conversao em repo publico

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Controllers\
+
+    class IndexController {
+
+        public function index() {
+            echo 'Chegou ao IndexController e disparou a action index';
+        }
+
+        public function sobreNos() {
+            echo 'Chegou ao IndexController e disparou a action sobreNos';
+        }
+
+    }
+
+?>
