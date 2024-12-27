5c135d3866dfafc9caf921073c95b56a87540fa7
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-12-27 20:32:17 -0300

    criação das Views

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,13 +4,32 @@
 
     class IndexController {
 
+        private $view;
+
+        public function __construct() {
+            $this->view = new \stdClass(); //classe vazia para passar informações para a view 
+        }
+
         //cada método representa uma action dos routes
         public function index() {
-            echo 'Chegamos ao IndexController e disparamos a action index <br>';
+
+            $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+            $this->render('index');
         }
 
         public function sobreNos() {
-            echo 'Chegamos ao IndexController e disparamos a action sobreNos <br>';
+            $this->view->dados = array('Notebook', 'Smartphone');
+            $this->render('sobreNos');
+        }
+
+        public function render($view) { //renderiza o view desejado e passa os dados para ele
+            //echo get_class($this);
+            $classeAtual = get_class($this);
+            $classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
+            $classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
+            
+            require_once "../App/Views/".$classeAtual."/".$view.".phtml"; //Lembrete: a referencia para requires está no contexto de "index.php"
+            //Está é uma forma bem dinamica de recuperar as views dentro do controlador
         }
 
     }
