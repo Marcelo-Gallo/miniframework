2a0d5d72686fbf135c9d2abaf8934974251aee51
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-12-28 10:09:35 -0300

    Defininfo leyouts reutilizaveis

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -9,12 +9,12 @@
         public function index() {
 
             $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
-            $this->render('index');
+            $this->render('index', 'layout1');
         }
 
         public function sobreNos() {
             $this->view->dados = array('Notebook', 'Smartphone');
-            $this->render('sobreNos');
+            $this->render('sobreNos', 'layout1');
         }
 
     }
