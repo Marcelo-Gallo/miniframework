c86aeede71ff79bfcfd8334aa096576f04e218a0
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-12-28 10:12:49 -0300

    upload de itens anteriormente ignorados pelo git

---
+++ vendor/MF/Controller/Action.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MF\Controller;
+
+abstract class  Action {
+    protected $view;
+
+    public function __construct() {
+        $this->view = new \stdClass(); //classe vazia para passar informações para a view 
+    }
+
+    protected function render($view, $layout) {
+        $this->view->page = $view;
+        if(file_exists("../App/Views/".$layout.".phtml")){
+            require_once("../App/Views/".$layout.".phtml");
+        } else {
+            $this->content();
+        }
+    }
+
+    protected function content() {
+        $classeAtual = get_class($this);
+        $classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
+        $classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
+        
+        require_once "../App/Views/".$classeAtual."/".$this->view->page.".phtml";
+    }
+}
